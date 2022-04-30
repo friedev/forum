d1a5e51ce6c86bfaf1e5314eb218cfa8687d1ba0
Aaron Friesen <maugrift@maugrift.com>
2022-04-30 16:13:57 -0500

    header: use consistent HTML concatenation

--- include/header.php
+++ include/header.php
@@ -6,14 +6,24 @@
 				<?php
 					if (empty($_SESSION['username'])) {
 						echo
-							'<a href="/login">Log In</a> '
-							. '<a class="button" href="/register">Register</a>';
+							'<a href="/login">'
+							. 'Log In'
+							. '</a> '
+							. '<a class="button" href="/register">'
+							. 'Register'
+							. '</a>';
 					} else {
 						echo
 							'Logged in as '
 							. $_SESSION['displayname']
-							. ' (<a href="/logout">Log Out</a>) '
-							. '<a class="button" href="/new">New Topic</a>';
+							. ' ('
+							. '<a href="/logout">'
+							. 'Log Out'
+							. '</a>'
+							. ') '
+							. '<a class="button" href="/new">'
+							. 'New Topic'
+							. '</a>';
 					}
 				?>
 			</p>
