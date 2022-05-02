bc66e03a704addf3194df91abccd086ee4bf63d1
Aaron Friesen <maugrift@maugrift.com>
2022-05-01 21:51:17 -0500

    Improve login and register pages

--- login/index.php
+++ login/index.php
@@ -8,6 +8,7 @@
 	<body>
 		<?php include $_SERVER['DOCUMENT_ROOT'] . '/include/header.php' ?>
 		<main>
+			<h1>Login</h1>
 			<form action="submit.php" method="post">
 				<label>Username</label><br />
 				<input class="text" type="text" name="username" required autofocus /><br />
@@ -15,6 +16,7 @@
 				<input class="text" type="password" name="password" required /><br />
 				<input class="submit" type="submit" name="submit" value="Log In" />
 			</form>
+			<p>Don't have an account? <a href="/register">Register.</a></p>
 		</main>
 <!--#include virtual="/include/footer.html"-->
 	</body>
--- register/index.php
+++ register/index.php
@@ -8,6 +8,7 @@
 	<body>
 		<?php include $_SERVER['DOCUMENT_ROOT'] . '/include/header.php' ?>
 		<main>
+			<h1>Register</h1>
 			<form action="submit.php" method="post">
 				<label>Username</label><br />
 				<input class="text" type="text" name="username" required autofocus /><br />
@@ -17,6 +18,7 @@
 				<input class="text" type="text" name="displayname" required /><br />
 				<input class="submit" type="submit" name="submit" value="Register" />
 			</form>
+			<p>Already have an account? <a href="/login">Log in.</a></p>
 		</main>
 <!--#include virtual="/include/footer.html"-->
 	</body>
